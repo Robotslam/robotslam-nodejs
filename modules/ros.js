792b0a91464fea4e293876e098a72bc0e1823c03
Andreas Coroiu <andreas.coroiu@gmail.com>
2017-02-13 11:15:31 +0100

    New file without obvious syntax errors.

--- modules/ros.js
+++ modules/ros.js
@@ -1,8 +1,10 @@
+var URL = require('url-parse');
 var roslib = require('roslib');
 var WiFiScanner = require('./modules/wifi');
 
-var ros_hostname = 
+var ros_uri = new URL(process.env['ROS_MASTER_URI']);
 var ros = new ROSLIB.Ros({
-    url : 'ws://'+ process.env['ROS_MASTER_URI'] +':9090'
+    url : 'ws://'+ ros_uri.hostname +':9090'
 });
+
 var wifi_scanner = WiFiScanner(ros);
