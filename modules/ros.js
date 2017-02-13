83f078e85038bf489dc5e0383e38fb197afb8f92
Andreas Coroiu <andreas.coroiu@gmail.com>
2017-02-13 11:16:05 +0100

    Connection thingies.

--- modules/ros.js
+++ modules/ros.js
@@ -7,4 +7,16 @@ var ros = new ROSLIB.Ros({
     url : 'ws://'+ ros_uri.hostname +':9090'
 });
 
+ros.on('connection', function() {
+	console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+	console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+	console.log('Connection to websocket server closed.');
+});
+
 var wifi_scanner = WiFiScanner(ros);
