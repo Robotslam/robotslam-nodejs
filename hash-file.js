a06b667a5d45d6c021bce42434438b1cc03c9a92
Andreas Coroiu <andreas.coroiu@gmail.com>
2017-02-16 14:54:00 +0100

    Minor fixes.

--- hash-file.js
+++ hash-file.js
@@ -18,13 +18,13 @@ const localPointMatrix = math.transpose(math.matrix(localPoints)).resize([3, 3],
 const globalPointMatrix = math.transpose(math.matrix(referencePoints)).resize([3, 3], 1);
 const transformationMatrix = math.multiply(globalPointMatrix, math.inv(localPointMatrix));
 const tM = transformationMatrix.toArray();
-var m = Matrix.from( tM[0][0], tM[1][0], tM[0][1], tM[1][1], tM[0][2], tM[1][2] );
+const m = Matrix.from( tM[0][0], tM[1][0], tM[0][1], tM[1][1], tM[0][2], tM[1][2] );
 
 for (let i = 0; i <= 1; i += 0.1) {
   result = m.applyToPoint( i, i );
   console.log(result.x + ',' + result.y);
 }
 
-for (var i = referencePoints.length - 1; i >= 0; i--) {
+for (let i = referencePoints.length - 1; i >= 0; i--) {
   console.log(referencePoints[i][0] + ',' + referencePoints[i][1]);
 }
