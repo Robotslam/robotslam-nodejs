e2905505158b1bba39c2d14af526dbe1c8fe7e0b
Hinton <oscar@combain.com>
2017-02-16 15:51:34 +0100

    Convert callback hell to promise-heaven.

--- routes/gps_convert.js
+++ routes/gps_convert.js
@@ -1,11 +1,11 @@
+const bluebird = require('bluebird');
+const fs = require('fs-promise');
 const express = require('express');
 const router = express.Router();
 const ros = require('../modules/ros');
-const base64 = require('node-base64-image');
 const gm = require('gm').subClass({imageMagick: true});
 const yaml = require('js-yaml');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('gps_convert/index', {
     title: 'GPS Convert Upload'
@@ -18,38 +18,52 @@ router.post('/fit_on_map', function (req, res, next) {
     return;
   }
 
-  req.files.map_description.mv('/tmp/map_description.yaml', function(err) {
-    if (err) {
-      res.status(500).send(err); // .....
-    } else {
-      description = yaml.load(req.files.map_description.data); // watch out, maybe use safe load?
-      req.files.map_image.mv('/tmp/map_image.pgm', function(err) {
-        if (err) {
-          res.status(500).send(err); // japp
-        } else {
-          gm('/tmp/map_image.pgm').transparent('#CDCDCD').write('/tmp/map_image.png', function (err) {
-            if (err) {
-              res.status(500).send(err); // hemskt
-            } else {
-              base64.encode('/tmp/map_image.png', {string: true, local: true}, function (err, img_res) {
-                if (err) {
-                  res.status(500).send(err); // absolut horribelt
-                } else {
-                  res.render('gps_convert/fit_on_map', {
-                    title: 'Fit on map',
-                    map_image_data: 'data:image/x-portable-graymap;base64,' + img_res,
-                    gps_references: description.gps_references,
-                    origin: description.origin,
-                    resolution: description.resolution
-                  });
-                }
-              });
-            }
-          });
-        }
+  return Promise
+    .all([
+      fs.writeFile('/tmp/map_description.yaml', req.files.map_description.data),
+      storeImage(req.files.map_image.data)
+    ])
+    .then((data) => {
+      const image = data[1];
+      const description = yaml.safeLoad(req.files.map_description.data);
+
+      res.render('gps_convert/fit_on_map', {
+        title: 'Fit on map',
+        map_image_data: 'data:image/x-portable-graymap;base64,' + image,
+        gps_references: description.gps_references,
+        origin: description.origin,
+        resolution: description.resolution
       });
-    }
-  });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
 });
+
+function storeImage(data) {
+  return Promise.resolve()
+    .then(() => {
+      return fs.writeFile('/tmp/map_image.pgm', data);
+    })
+    .then(() => {
+      return new Promise((fulfill, reject) => {
+        gm('/tmp/map_image.pgm')
+          .transparent('#CDCDCD')
+          .write('/tmp/map_image.png', function (err) {
+            if (err) {
+              return reject(err);
+            }
+            return fulfill(err);
+          });
+      });
+    })
+    .then(() => {
+      return fs.readFile('/tmp/map_image.png');
+    })
+    .then((image) => {
+      return new Buffer(image).toString('base64');
+    });
+}
 
 module.exports = router;
