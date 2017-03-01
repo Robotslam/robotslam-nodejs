17a5d232cd8ba1538d14ed6a4680f70c4aaee912
Hinton <oscar@combain.com>
2017-03-01 10:26:14 +0100

    Move routes into seperate file

---
+++ routes/home.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const ros = require('../modules/ros');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  console.log(ros.active);
+  res.render('index', {
+    title: 'Robotslam',
+    active: ros.active
+  });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var router = express.Router();
-const ros = require('../modules/ros');
+const express = require('express');
+const router = express.Router();
 
+const home = require('./home');
+const users = require('./users');
+const toggle = require('./toggle');
+const gps_convert = require('./gps_convert');
+const exportRoute = require('./export');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  console.log(ros.active);
-  res.render('index', {
-    title: 'Express',
-    active: ros.active
-  });
-});
+router.use('/', home);
+router.use('/users', users);
+router.use('/toggle', toggle);
+router.use('/gps_convert', gps_convert);
+router.use('/export', exportRoute);
 
 module.exports = router;
