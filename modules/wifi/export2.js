3d1951f40d1b8441cd8e7e73add085338b7ec619
Hinton <oscar@combain.com>
2017-03-20 12:58:01 +0100

    Add export to csv functionality to sql stored data

---
+++ modules/wifi/export2.js
@@ -0,0 +1,114 @@
+const crypto = require('crypto');
+const config = require('config');
+const csv_string = require('./csv_string');
+
+const KEY = config.get('export.key');
+const SALT = config.get('export.salt');
+
+/**
+ * Accepts the WiFi scan message, and formats it into correct csv format
+ */
+function exportCsv(points, transformer) {
+
+  const out = [];
+
+  let p = Promise.resolve();
+
+  let i = 1;
+
+  // Iterate over each measurement time
+  points.forEach((point) => {
+
+    // Ensure we actually have some data to export
+    if (point.measurementPointWifis.length <= 0) {
+      return;
+    }
+
+    p = p.then(() => {
+      const formattedMsg = formatPoint(point, transformer);
+      return hash(formattedMsg, i++).then((h) => {
+        out.push(...formattedMsg);
+        out.push(h);
+        out.push(['-- New Line --']);
+      });
+    });
+  });
+
+  return p.then(() => {
+    return csv_string(out);
+  });
+}
+
+function hash(msg, i) {
+  return csv_string(msg)
+    .then((data) => {
+      const hmac = crypto.createHmac('sha256', KEY);
+      const o = hmac.update(`robotslamimei;${i};${data};${SALT}`).digest('hex');
+
+      return ['H', i, o];
+    });
+}
+
+function formatPoint(point, transformer) {
+  const measurements = point.measurementPointWifis.map(formatMeasurement);
+
+  const out = [];
+
+  out.push(formatHeader(point, transformer));
+  out.push(['AV', '1.0', 1]);
+  out.push([]);
+  out.push(...measurements);
+  out.push([]);
+
+  return out;
+}
+
+function formatMeasurement(val) {
+  return [
+    'W',       // WiFi / BT?
+    val.bssid, // BSSID
+    val.ssid,  // SSID
+    val.rssi,  // RSSID
+    '',        // Capabilities (Encryption Type?)
+    '',        // Frequency
+    val.age,       // Age
+    '',        // Venue Name    (Only available on passpoint network)
+    ''         // Operator Name (Only available on passpoint network)
+  ];
+}
+
+function formatHeader(point, transformer) {
+  const position = transformer.transformPoint(point.x, point.y);
+  return [
+    1,              // Valid?
+    position.x, // Latitude
+    position.y, // Longitude
+    0.1,            // Accuracy
+    0,              // Altitude
+    0,              // Speed
+    0,              // Bearing
+    0,              // Age of location
+    '',
+    '',
+    'f',
+    'f',
+    0,
+    point.time, // Last scantime
+    'robotslamimei', // Unique identifier for device
+    'RobotSlam', // Application name
+    '',
+    '',
+    0,  // WiFi getLocation?
+    0,  // noLearning?
+    '',
+    0,  // refPoints
+    '',
+    '',
+    '', // Room
+    '', // Accuracy?
+    '',
+    '',
+  ];
+}
+
+module.exports = exportCsv;
--- routes/measurements.js
+++ routes/measurements.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const yaml = require('js-yaml');
 const models = require('../models');
+const Transformer = require('../modules/transformer');
+const exportCsv = require('../modules/wifi/export2');
 
 router.get('/', async function (req, res) {
   //noinspection JSUnresolvedVariable,JSUnresolvedFunction
@@ -10,6 +13,30 @@ router.get('/', async function (req, res) {
     title: 'Measurements',
     measurements: measurements,
   });
+});
+
+router.get('/:id/export', async function (req, res) {
+  const measurement = await models.measurement.find(res.id);
+
+  res.render('measurements/export', {
+    title: 'Measurements',
+    measurement: measurement
+  });
+});
+
+router.post('/:id/export', async (req, res) => {
+
+  const points = await models.measurementPoint.findAll({
+    include: [models.measurementPointWifi]
+  });
+
+  const description = yaml.load(req.files.map_description.data);
+  const transformer = new Transformer(description);
+  const output = await exportCsv(points, transformer);
+
+  //res.setHeader('content-disposition', 'attachment; filename=' + newFilename);
+  res.setHeader('content-type', 'text/plain');
+  res.send(output);
 });
 
 module.exports = router;
