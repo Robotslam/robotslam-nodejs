cc6c7bb70811dd2c65c7aab571c58b128d4fbc82
Hinton <oscar@combain.com>
2017-03-28 12:52:46 +0200

    Remove unused routes.

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,12 @@ const router = express.Router();
 
 const home = require('./home');
 const users = require('./users');
-const gps_convert = require('./gps_convert');
-const exportRoute = require('./export');
 const measurements = require('./measurements');
 const buildings = require('./buildings');
 const map = require('./map');
 
 router.use('/', home);
 router.use('/users', users);
-router.use('/gps_convert', gps_convert);
-router.use('/export', exportRoute);
 router.use('/measurements', measurements);
 router.use('/buildings', buildings);
 router.use('/maps', map);
