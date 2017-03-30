d6a6f82cda147924ab38324a8a8674e8d55f2348
Hinton <oscar@combain.com>
2017-03-30 10:31:55 +0200

    Minor design improvements with button spacing. Add pretty print of html for dev env.

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const app = express();
 app.locals.moment = require('moment');
 app.locals.moment.locale('sv');
 
+if (app.get('env') !== 'production') {
+  app.locals.pretty = true;
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
